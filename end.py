17e82b7353c511b70dd40bcd47e7c2919e07456a
Your Name <you@example.com>
2024-08-14 23:49:07 +0000

    -

--- end.py
+++
@@ -1,4 +0,0 @@
-import struct
-
-print(struct.calcsize("f"))  # prints 4 for LE and 8 for BE
-
--- print_str.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-  char msg[] = "We are going to make it";
-  printf("%s\n", msg);
-  return 0;
-}
