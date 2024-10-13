4bd20575e216dd7072840480f42a6c9dc2cb90ba
Your Name <you@example.com>
2024-10-13 11:48:42 +0000

    -

--- intro_to_x64/fasm_run.sh
+++ intro_to_x64/fasm_run.sh
@@ -1,3 +1,10 @@
+;
+;	echo_fun.asm
+;	intro_to_x64 
+; complies and runs assembly file written for fasm assembler
+;
+;	Created by Vrindavan Sanap on 2024-10-13.
+;	
 fasm "$1"
 filename="${1%.*}"
 
